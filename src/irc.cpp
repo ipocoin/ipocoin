a83e54f4fada39e453e4d8ec6f45eef4b37eb374
R. Benson Evans <r.benson.evans@gmail.com>
2013-10-07 16:39:46 -0400

    converted SmallChange to IPOCoin.

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #smallchangeTEST3\r");
-            Send(hSocket, "WHO #smallchangeTEST3\r");
+            Send(hSocket, "JOIN #ipocoinTEST3\r");
+            Send(hSocket, "WHO #ipocoinTEST3\r");
         } else {
-            // randomly join #smallchange00-#smallchange99
+            // randomly join #ipocoin00-#ipocoin99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #smallchange%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #smallchange%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #ipocoin%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #ipocoin%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("ICN");
+    case mBTC: return QString("mICN");
+    case uBTC: return QString::fromUtf8("μICN");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("IPOCoin");
+    case mBTC: return QString("Milli-IPOCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-IPOCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
